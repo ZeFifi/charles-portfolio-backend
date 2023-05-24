3e94e71352cc980d7de2be43376e261c81c0a2eb
Philippe Pinceloup <ph.pinceloup@protonmail.com>
2023-05-24 11:16:09 +0200

    Update database

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
+      host: env("DATABASE_HOST", "dpg-chkvf6jhp8uej70katug-a"),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
+      database: env("DATABASE_NAME", "strapi_4rdd"),
       user: env("DATABASE_USERNAME", "strapi"),
-      password: env("DATABASE_PASSWORD", "strapi"),
+      password: env("DATABASE_PASSWORD", "0T5usF3cHk78luIezC067G4zEYjMGma7"),
       schema: env("DATABASE_SCHEMA", "public"), // Not required
       ssl: {
         ca: env(DATABASE_CA),
