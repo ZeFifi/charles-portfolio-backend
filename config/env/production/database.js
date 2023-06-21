9f03be091ef6338d58bdc9ffa26c74dbcf3a8cc8
Philippe Pinceloup <ph.pinceloup@protonmail.com>
2023-06-21 11:40:45 +0200

    Update SSL

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,14 +2,14 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "dpg-chkvf6jhp8uej70katug-a"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi_4rdd"),
-      user: env("DATABASE_USERNAME", "strapi"),
-      password: env("DATABASE_PASSWORD", "0T5usF3cHk78luIezC067G4zEYjMGma7"),
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
       schema: env("DATABASE_SCHEMA", "public"), // Not required
       ssl: {
-        ca: env(DATABASE_CA),
+        ca: env("DATABASE_SSL_CA", undefined),
       },
     },
     debug: false,
